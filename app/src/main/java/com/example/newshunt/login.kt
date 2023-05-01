ae28ace517adf972da4025cb05ef73326208ac2a
ishant <ishantkuwt@gmail.com>
2023-05-01 16:16:17 +0530

    done

---
+++ app/src/main/java/com/example/newshunt/login.kt
@@ -0,0 +1,64 @@
+package com.example.task_geek_camp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import com.androiddevs.mvvmnewsapp.mm
+import com.example.newshunt.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        //#0047C1
+
+        val fp = findViewById<TextView>(R.id.fp)
+        val huh = findViewById<ImageView>(R.id.huh)
+        val logo = findViewById<AppCompatButton>(R.id.logo)
+        val roll = findViewById<EditText>(R.id.roll)
+        val pas = findViewById<EditText>(R.id.pas)
+        huh.setOnClickListener{
+            Toast.makeText(this,"Press me more",Toast.LENGTH_SHORT).show()
+        }
+        fp.setOnClickListener{
+            Toast.makeText(this,"Remember your Password",Toast.LENGTH_LONG).show()
+        }
+        roll.setOnClickListener {
+            roll.text = null
+        }
+        pas.setOnClickListener {
+            pas.text = null
+        }
+        logo.setOnClickListener{
+            val ds :String = roll.text.toString()
+            val ds2 :String = pas.text.toString()
+            if(ds=="Enter your enrollment number" || ds2=="Password"){
+                Toast.makeText(this,
+                    "Please Enter Your Credentials",
+                    Toast.LENGTH_LONG).show()
+            }
+            else if(ds.isEmpty() || ds2.isEmpty()){
+                Toast.makeText(this,
+                    "Please Enter Your Credentials",
+                    Toast.LENGTH_LONG).show()
+            }
+
+            else{
+                val intent = Intent(this,mm::class.java)
+                startActivity(intent)
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/newshunt/mm.kt
@@ -0,0 +1,12 @@
+package com.androiddevs.mvvmnewsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newshunt.R
+
+class mm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mm)
+    }
+}
